b46a4e4cb3db9de1a72ebc8a6409d08400d0fa9d
Rinto Jose <rintoj@gmail.com>
2025-01-22 06:33:32 +0530

    [0m"Refactor schema to replace Account type with User, update resolvers and servers to use new type"[0m

--- graphql/src/main.ts
+++ graphql/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(config.PORT);
+  console.log(
+    `\nServer is running at: ${(await app.getUrl()).replace('[::1]', 'localhost')}${config.GRAPHQL_PATH}`,
+  );
 }
 bootstrap();
