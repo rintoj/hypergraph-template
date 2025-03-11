2a56b1db86f01ddbce9551be79df15c9a8c35375
Rinto Jose <rintoj@gmail.com>
2025-03-11 19:53:08 +0530

    feat: Setup basic GraphQL NestJS app with Auth and Storage modules

--- graphql/src/app.module.ts
+++ graphql/src/app.module.ts
@@ -1,3 +1,6 @@
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { AuthModule } from '@hgraph/auth';
+import { createLocalStrategy } from '@hgraph/auth/local';
 import { RepositoryType, StorageModule } from '@hgraph/storage/nestjs';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
@@ -6,15 +9,19 @@ import { AppController } from './app.controller';
 import { config } from './config';
 import { UserModule } from './user/user.module';
 import { UserService } from './user/user.service';
-import { AuthModule } from '@hgraph/auth';
-import { createLocalStrategy } from '@hgraph/auth/local';
-// import { createSupabaseAuthStrategy } from '@hgraph/auth/supabase';
+
+const APP_MODULES = [UserModule];
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      playground: config.GRAPHQL_PLAYGROUND,
+      playground: false,
+      plugins: [
+        config.GRAPHQL_PLAYGROUND
+          ? ApolloServerPluginLandingPageLocalDefault()
+          : undefined,
+      ],
       autoSchemaFile: config.SCHEMA_FILE,
       path: config.GRAPHQL_PATH,
       introspection: true,
@@ -24,14 +31,7 @@ import { createLocalStrategy } from '@hgraph/auth/local';
     }),
     StorageModule.forRoot({
       repositoryType: RepositoryType.TypeORM,
-      url: config.DATABASE_URL,
     }),
-    // StorageModule.forRoot({
-    //   repositoryType: RepositoryType.Firestore,
-    //   serviceAccountConfig: config.FIREBASE_SERVICE_ACCOUNT,
-    //   storageBucket: config.FIREBASE_STORAGE_BUCKET,
-    // }),
-    UserModule,
     AuthModule.forRoot({
       userService: UserService,
       strategies: [
@@ -39,12 +39,6 @@ import { createLocalStrategy } from '@hgraph/auth/local';
           enableGraphQLAPI: true,
           enableRestAPI: true,
         }),
-        // createSupabaseAuthStrategy({
-        //   supabaseUrl: config.SUPABASE_URL,
-        //   supabaseAnonKey: config.SUPABASE_ANON_KEY,
-        //   redirectUrl: config.AUTH_REDIRECT_URL,
-        //   providers: ['google', 'github', 'facebook'],
-        // }),
       ],
       jwtConfig: {
         secret: config.JWT_SECRET,
@@ -53,6 +47,7 @@ import { createLocalStrategy } from '@hgraph/auth/local';
         refreshExpiry: config.JWT_REFRESH_EXPIRY,
       },
     }),
+    ...APP_MODULES,
   ],
   controllers: [AppController],
 })
--- graphql/src/config.ts
+++ graphql/src/config.ts
@@ -1,11 +1,7 @@
 import * as dotenv from 'dotenv';
 import { bool, cleanEnv, num, str } from 'envalid';
 
-const configFile =
-  process.env.NODE_ENV !== 'production'
-    ? `.env.${process.env.NODE_ENV ?? 'local'}`
-    : '.env';
-
+const configFile = '.env';
 console.log(`Loading config from "${configFile}"`);
 
 dotenv.config({
@@ -26,11 +22,10 @@ export const config = cleanEnv(process.env, {
   JWT_REFRESH_SECRET: str(),
   JWT_REFRESH_EXPIRY: str(),
 
-  DATABASE_URL: str(),
-  DATABASE_TYPE: str(),
-  DB_SYNCHRONIZE: bool({ default: false, devDefault: true }),
-
-  SUPABASE_URL: str(),
-  SUPABASE_ANON_KEY: str(),
-  AUTH_REDIRECT_URL: str(),
+  DATABASE_HOST: str(),
+  DATABASE_PORT: num(),
+  DATABASE_NAME: str(),
+  DATABASE_USER: str(),
+  DATABASE_PASSWORD: str(),
+  DATABASE_SYNCHRONIZE: bool({ default: false }),
 });
